f8d07ead47d3d5873f07d310f868038d1e788f6c
Tiago Faxina <thfxina@gmail.com>
2021-03-31 20:36:52 -0300

    Add getuser by full name

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { GamesRepository } from "../modules/games/repositories/implementations/GamesRepository";
+import { UsersRepository } from "../modules/users/repositories/implementations/UsersRepository";
 
 const usersRoutes = Router();
 
@@ -9,6 +10,19 @@ usersRoutes.get(
     const { gameId } = request.params;
     const gamesRepository = new GamesRepository();
     const amontOfgames = await gamesRepository.findUsersByGameId(gameId);
+    return response.status(200).send(amontOfgames);
+  }
+);
+
+usersRoutes.get(
+  "/first-name/:firstName/last-name/:lastName",
+  async (request: Request, response: Response) => {
+    const { firstName, lastName } = request.params;
+    const gamesRepository = new UsersRepository();
+    const amontOfgames = await gamesRepository.findUserByFullName({
+      first_name: firstName,
+      last_name: lastName,
+    });
     return response.status(200).send(amontOfgames);
   }
 );
